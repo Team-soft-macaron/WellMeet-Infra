2598dadd7cdd7cd0883e9426c1e7f6a463bd2904
ez <105545215+ezcolin2@users.noreply.github.com>
2025-07-14 16:13:09 +0900

    fix: save reviews lambda function에게 S3 접근 권한 부여 (#4)

--- variables.tf
+++ variables.tf
@@ -15,39 +15,7 @@ variable "aws_region_env" {
   type        = string
 }
 
-# variable "ec2_key_name" {
-#   description = "Name of the SSH key pair to use for EC2 instance."
-#   type        = string
-# }
-
-# variable "ec2_ami_id" {
-#   description = "AMI ID for Ubuntu latest to use for EC2 instance."
-#   type        = string
-# }
-
-# variable "ec2_instance_profile_arn" {
-#   description = "IAM instance profile ARN for EC2 (for ECR/ECS access, etc)"
-#   type        = string
-#   default     = ""
-# }
-
-# variable "atmosphere_role_arn" {
-#   description = "ARN of the Lambda execution role for review classifier"
-#   type        = string
-# }
-
-# variable "atmosphere_image_uri" {
-#   description = "ECR image URI for review classifier Lambda"
-#   type        = string
-# }
-
-# variable "atmosphere_output_bucket_name" {
-#   description = "Name of the S3 bucket to upload classified reviews (e.g., naver-map-review-atmosphere)"
-#   type        = string
-# }
-
-# variable "atmosphere_function_name" {
-#   description = "Name of the atmosphere Lambda function"
-#   type        = string
-#   default     = "atmosphere-classifier"
-# }
+variable "api_url" {
+  description = "API URL for Lambda environment variable"
+  type        = string
+}
